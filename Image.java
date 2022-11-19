236c4d282aa51d9502582b6f4e9b5fd60d33558d
Skyrdow <lucas.mesias@usach.cl>
2022-11-18 22:25:40 -0300

    Agregar funciones y pixmap

--- Image.java
+++ Image.java
@@ -78,4 +78,38 @@ public class Image {
 		return false;
 	}
 	
+	public void flipH()
+	{
+		for (Pixel p : this.pixeles)
+		{
+			p.setX(p.getX() - this.width + 1);
+		}
+	}
+	
+	public void flipV()
+	{
+		for (Pixel p : this.pixeles)
+		{
+			p.setY(p.getY() - this.height + 1);
+		}
+	}
+	
+	public void crop(int x1, int y1, int x2, int y2)
+	{
+		ArrayList<Pixel> newPixs = new ArrayList<Pixel>();
+		for (Pixel p : this.pixeles)
+		{
+			if (x1 <= p.getX() && p.getX() <= x2 &&
+				y1 <= p.getY() && p.getY() <= y2)
+				newPixs.add(p);
+		}
+		this.setPixeles(newPixs);
+		this.setWidth(x2 - x1 + 1);
+		this.setHeight(y2 - y1 + 1);
+	}
+	
+	public void histogram()
+	{
+		return;
+	}
 }
---
+++ Pixmap.java
@@ -0,0 +1,11 @@
+package interfaz;
+
+import java.util.ArrayList;
+
+public class Pixmap extends Image {
+
+	public Pixmap(int width, int height, ArrayList<Pixel> pixeles) {
+		super(width, height, pixeles);
+	}
+	
+}
